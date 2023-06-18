a75f6daa54ef34dfae692a3c8752f39499c4d43b
Thoss24 <t.fogarty24@outlook.com>
2023-06-18 13:45:57 +0100

    more custom hook practice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const modifyTasks = tasksData => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksData) {
+      loadedTasks.push({ id: taskKey, text: tasksData[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+
+  const { isLoading, error, sendRequest: fetchTasks} = useHttp({url: 'https://custom-hooks-react-http-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'}, modifyTasks)
+
+  // useEffect(() => {
+  //   fetchTasks()
+  // }, [fetchTasks])
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
     <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
     </React.Fragment>
   );
-}
+  }
 
 export default App;
